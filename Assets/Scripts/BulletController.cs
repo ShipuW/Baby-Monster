4bcd0df50b9286e7fa69e2c058344258cc9054bb
Hao Wu <hwu638@usc.edu>
2017-10-14 14:29:21 -0700

    add new moving and skill joysticks

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	public Rigidbody2D supply; 
+	public int damage = 10;
+	
+	void Start () 
+    {
+        Destroy(gameObject, 5);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll) 
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (coll.gameObject.CompareTag("BreakableBox"))
+        {
+        	Rigidbody2D supplyInstance = Instantiate(supply, coll.transform.position, new Quaternion(0, 0, 0, 0)) as Rigidbody2D;
+            Destroy(coll.gameObject);
+            Destroy(gameObject);
+        } else if (coll.gameObject.CompareTag("Monster")) {
+            //Destroy(coll.gameObject);
+			MonsterController monster = coll.gameObject.GetComponent<MonsterController>();
+			monster.takeDamage (damage);
+			Debug.Log ("Enemy got hit!");
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Assets/Scripts/JFTouch.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;  
+using System.Collections;  
+
+public class JFTouch  {  
+
+	public static bool TouchBegin()  
+	{  
+		if(Input.GetMouseButtonDown(0))  
+		{  
+			return true;  
+		}  
+		else if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)  
+		{  
+			return true;  
+		}  
+		return false;  
+	}  
+
+	public static bool TouchEnd()  
+	{  
+		if(Input.GetMouseButtonUp(0))  
+		{  
+			return true;  
+		}  
+		else if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)  
+		{  
+			return true;  
+		}  
+		return false;  
+
+	}  
+
+	public static bool TouchIng()  
+	{  
+		if(Input.GetMouseButton(0))  
+		{  
+			return true;  
+		}  
+		else if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)  
+		{  
+			return true;  
+		}  
+		return false;  
+	}  
+
+}  
