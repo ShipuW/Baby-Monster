733547bf8275ff738ceba3020d7a63135deb0a72
Shipu Wang <shipu.wsp@gmail.com>
2017-10-14 16:13:09 -0700

    add explosion effect

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,15 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BulletController : MonoBehaviour {
 
 	public Rigidbody2D supply; 
 	public int damage = 10;
-	
+    public GameObject explosion;
+    public float radius;
+
+
+
+    public static IEnumerator DelayToInvokeDo(Action action, float delaySeconds)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        action(); //最好IF判断一下不为空
+    }
+
 	void Start () 
     {
-        Destroy(gameObject, 5);
+
+        StartCoroutine(DelayToInvokeDo(() =>
+        {
+            if(gameObject == null) {
+                return;
+            }
+
+            GameObject explodeEffect = Instantiate(explosion,gameObject.transform.position,Quaternion.identity);  
+            //获得以炸弹为中心的一定范围内的所有对象  
+            // ParticleSystem hitParticles = explodeEffect.transform.GetChild(0).GetComponentInChildren<ParticleSystem>();
+            // hitParticles.play();
+            Destroy(gameObject);
+            Collider2D[] colliders= Physics2D.OverlapCircleAll(gameObject.transform.position,radius);
+            //如果炸弹碰到的是砖块，则销毁砖块  
+            foreach(Collider2D collider in colliders){  
+                string co_tag=collider.gameObject.tag;  
+                if(co_tag == "BreakableBox" || co_tag == "Monster"){  
+                    Destroy(collider.gameObject);  
+                }  
+            } 
+            
+            
+        }, 2.0f));
     }
 
 
