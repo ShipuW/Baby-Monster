d76fb3fd7e8ff9df0029849a0b2b99c94cb322df
Shipu Wang <shipu.wsp@gmail.com>
2017-10-15 12:33:04 -0700

    merge joystick

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
 using System;
-=======
->>>>>>> serverJoy
 
 public class BulletController : MonoBehaviour {
 
 	public Rigidbody2D supply; 
 	public int damage = 10;
-<<<<<<< HEAD
     public GameObject explosion;
     public float radius;
 
@@ -47,12 +43,6 @@ public class BulletController : MonoBehaviour {
             
             
         }, 2.0f));
-=======
-	
-	void Start () 
-    {
-        Destroy(gameObject, 5);
->>>>>>> serverJoy
     }
 
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,32 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;             //Floating point variable to store the player's movement speed.
-<<<<<<< HEAD
-    public float bulletSpeed = 2f; 
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-    public Rigidbody2D Bullet;                // 子弹 Prefab
 
-    public VJHandler jsMovement;
-    private Vector3 direction;
-	private PlayerHealth health;
-
-    void OnEnable(){
-		EasyButton.On_ButtonUp += On_ButtonUp;
-	}
-	void On_ButtonUp(string buttonName){
-
-		if(buttonName.Equals("put_down")){
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-
-		//Store the current vertical input in the float moveVertical.
-		float moveVertical = Input.GetAxis ("Vertical");
-			// 初始化子弹
-		Rigidbody2D bulletInstance = Instantiate(Bullet, transform.position, new Quaternion(0, 0, 0, 0)) as Rigidbody2D;
-			// velocity直接给物体一个固定的移动速度
-		bulletInstance.velocity = new Vector2(moveHorizontal * bulletSpeed, moveVertical * bulletSpeed);
-		}
-	}
-=======
     public float bulletSpeed = 10f; 
     private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
     public Rigidbody2D Bullet;                // 子弹 Prefab
@@ -75,60 +50,12 @@ public class PlayerController : MonoBehaviour {
 		bulletPosition = new Vector2 (transform.position.x + move.x*10, transform.position.y + move.y*10);
 	}
 
->>>>>>> serverJoy
     // Use this for initialization
     void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D> ();
 		health = GetComponent<PlayerHealth> ();
-<<<<<<< HEAD
-    }
-
-    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
-    void FixedUpdate()
-    {
-
-        Vector2 movement;
-
-        direction = jsMovement.InputDirection; 
-
-        if (direction != Vector3.zero) 
-        {
-            movement = new Vector2 (direction.x, direction.y);
-        } else {
-            //Store the current horizontal input in the float moveHorizontal.
-            float moveHorizontal = Input.GetAxis ("Horizontal");
-
-            //Store the current vertical input in the float moveVertical.
-            float moveVertical = Input.GetAxis ("Vertical");
-
-            //Use the two store floats to create a new Vector2 variable movement.
-            movement = new Vector2 (moveHorizontal, moveVertical);
-        }
-
-        
-
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce (movement * speed);
-
-    }
-
-    void Update() 
-    {
-        // float moveHorizontal = Input.GetAxis ("Horizontal");
-
-        // //Store the current vertical input in the float moveVertical.
-        // float moveVertical = Input.GetAxis ("Vertical");
-        
-        // if(Input.GetButtonDown("Fire1") || (Input.GetKeyDown(KeyCode.Z)) == true)
-        // {
-        //     // 初始化子弹
-        //     Rigidbody2D bulletInstance = Instantiate(bullet, transform.position, new Quaternion(0, 0, 0, 0)) as Rigidbody2D;
-        //     // velocity直接给物体一个固定的移动速度
-        //     bulletInstance.velocity = new Vector2(moveHorizontal * bulletSpeed, moveVertical * bulletSpeed);
-        // }
-=======
 
     }
 
@@ -183,7 +110,6 @@ public class PlayerController : MonoBehaviour {
     void Update() 
     {
         
->>>>>>> serverJoy
     }
 
     void OnTriggerEnter2D(Collider2D other) 
