29f84d88539e9180f56d2b7500f550451f775c5a
Shipu Wang <shipu.wsp@gmail.com>
2017-11-13 15:13:36 -0800

    merge

--- Assets/Scripts/HoldingManager.cs
+++ Assets/Scripts/HoldingManager.cs
@@ -17,8 +17,8 @@ public class HoldingManager : MonoBehaviour {
 		
 	}
 
-	public pickupPiece (PickupType type) {
-		isHolding
-	}
+	// public pickupPiece (PickupType type) {
+	// 	isHolding = true;
+	// }
 
 }
